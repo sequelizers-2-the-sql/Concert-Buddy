20499c1bfa2a82f1d09a935061f7a4fcd1ac965a
richcoleman <coleman.richardm@gmail.com>
2019-02-02 13:42:12 -0600

    some basic changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    "testValue":"getting...."
+  }
+
+  componentDidMount() {
+    console.log("MountingAPP");
+    axios.get("/api/test").then(result => {
+      console.log(result.data.test);
+      this.setState({"testValue": result.data.test})
+    });
+
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the FINAL PROJECT</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          New project, y'all. {this.state.testValue}
         </p>
       </div>
     );
