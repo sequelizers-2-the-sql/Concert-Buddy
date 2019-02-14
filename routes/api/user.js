96c3ffbb7c6343ced22ce7594790b31bfcccc209
“IBayon“ <IBayon@Enova.com>
2019-02-13 21:42:09 -0600

    fixed path in models/api

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../../models/user.js')
+const User = require('../../models/User')
 const passport = require('../../passport')
 
 router.post('/signup', (req, res) => {
