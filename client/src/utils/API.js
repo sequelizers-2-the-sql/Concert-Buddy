31f541925daca8eada55ad7d0f76ba9f53af92b9
Andrew Andersen <andersac88@gmail.com>
2019-02-13 18:56:28 -0600

    Populate

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,8 @@ import axios from "axios";
 export default {
     attendConcert: function(eventData) {
         return axios.post("/api/concerts", eventData);
+    },
+    getConcert: function(id) {
+        return axios.get("/api/concerts/" + id);
     }
 }
