9fb6ffe6c6d018251055521bd7b2935348a69008
“IBayon“ <IBayon@Enova.com>
2019-02-13 22:00:19 -0600

    fixed

--- passport/index.js
+++ passport/index.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('./localStrategy')
-const User = require('../models/user.js')
+const User = require('../models/User')
 
 // called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
