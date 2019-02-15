df05ee93154e1c6253694a7ab8f6fd23bf3ced28
“IBayon“ <IBayon@Enova.com>
2019-02-14 20:30:10 -0600

    adding database connection for Heroku so that Rich doesn't bitch and complain that we don't have it running n Heroku

--- models/index.js
+++ models/index.js
@@ -2,9 +2,18 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-//your local database url
+//your local database url.  COMMENT OUT BEFORE pushing to Heroku!!!!
 //27017 is the default mongoDB port
-const uri = 'mongodb://127.0.0.1:27017/localCB' 
+//const uri = 'mongodb://127.0.0.1:27017/localCB' 
+
+//adding to fix the Heroku issue.
+//COMMENT OUT BEFORE using this locally and un comment above!!!!!
+const uri = process.env.uri || "mongodb://concertbuddy:concertbuddy2@ds123645.mlab.com:23645/heroku_phjs0vs3";
+
+mongoose.connect(uri,{ useNewUrlParser: true });
+
+//end adding to fix Heroku issue.  
+
 
 mongoose.connect(uri).then(
     () => { 
