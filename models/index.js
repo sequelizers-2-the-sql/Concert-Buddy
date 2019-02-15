9fa9b7c308ec8edab7c8002e3d96bd8ff7671190
“IBayon“ <IBayon@Enova.com>
2019-02-15 00:31:51 -0600

    file

--- models/index.js
+++ models/index.js
@@ -2,18 +2,18 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
+//adding to fix the Heroku issue.
+//COMMENT OUT BEFORE using this locally
+//const MONGODB_URI = process.env.MONGODB_URI || "mongodb://concerbuddy:concertbuddy2@ds123645.mlab.com:23645/heroku_phjs0vs3";
+//mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
+//end adding to fix Heroku issue.  
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1/localCB", { useNewUrlParser: true });
+
+
 //your local database url.  COMMENT OUT BEFORE pushing to Heroku!!!!
 //27017 is the default mongoDB port
 //const uri = 'mongodb://127.0.0.1:27017/localCB' 
-
-//adding to fix the Heroku issue.
-//COMMENT OUT BEFORE using this locally and un comment above!!!!!
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://concerbuddy:concertbuddy2@ds123645.mlab.com:23645/heroku_phjs0vs3";
-mongoose.connect(MONGODB_URI,{ useNewUrlParser: true });
-//end adding to fix Heroku issue.  
-
-
-
 //BELOW DOES THE MONGO CONNECTION (mongoose.connect)
 // mongoose.connect(uri).then(
 //     () => { 
