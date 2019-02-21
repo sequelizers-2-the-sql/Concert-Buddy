80192ef2af4990a4659dcf6f9729606be745998a
richcoleman <coleman.richardm@gmail.com>
2019-02-20 19:47:11 -0600

    deleted lock, added changes to css and js

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
+// import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
 
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+		<App />,
 	document.getElementById('root')
 )
