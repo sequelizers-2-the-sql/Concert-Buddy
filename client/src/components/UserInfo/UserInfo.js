65eb77904394017694e41e4545fdc1a093da48c2
richcoleman <coleman.richardm@gmail.com>
2019-02-23 12:45:20 -0600

    more changes to myevents page

--- client/src/components/UserInfo/UserInfo.js
+++ client/src/components/UserInfo/UserInfo.js
@@ -5,28 +5,46 @@ import "./UserInfo.css"
 
 export function UserEvents(props) {
     return (<>
-        <div className="card concert-card">
-            <div className="card-body">
-                <h5 className="card-title">{props.event.artist}</h5>
-                <p className="card-text">{props.event.venue}, {props.event.city}</p>
-            </div>
-            <ul className="list-group list-group-flush">
-                <li className="list-group-item">{props.event.date}</li>
-                <li className="list-group-item">{props.event.time}</li>
-                <li className="list-group-item">{props.event.attendees.length}</li>
-                <li className="list-group-item">tidbit about the band? (do we need a new api?)</li>
+        <div className="row">
+            <div className="col-md-6">
+                <div className="card concert-card">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.event.artist}</h5>
+                        <p className="card-text">{props.event.venue} in {props.event.city}</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">On {props.event.date}</li>
+                        <li className="list-group-item">At {props.event.time}</li>
+                        <li className="list-group-item">{props.event.attendees.length} buddies attending!</li>
+                        <li className="list-group-item">tidbit about the band? (do we need a new api?)</li>
 
-            </ul>
-            <div className="card-body">
-                <a href="" className="card-link">Find ConcertBuddies</a>
-                <a href="" className="card-link">Delete Event</a>
+                    </ul>
+                    <div className="card-body">
+                        <a href="" className="card-link">Find ConcertBuddies</a>
+                        <a href="" className="card-link">Delete Event</a>
+                    </div>
+                </div>
             </div>
+            <div className="col-md-6">
+                <div>
+                    <div class="card map-card">
+                        <img class="card-img-top" src=".../100px180/" alt="Card image cap"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Google Map goes here</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
+
     </>
     );
 }
 
-// not sure if we want to have the user's username/password/ or additional sign up fields 
+    // not sure if we want to have the user's username/password/ or additional sign up fields
 // export function UserDetails(props) {
 //     return (<>
 //         <div className="card concert-card">
