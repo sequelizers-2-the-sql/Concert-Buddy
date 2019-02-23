4154df22c0109a867880f58d31d53a80c4d2d076
richcoleman <coleman.richardm@gmail.com>
2019-02-23 13:32:03 -0600

    concert detail page updated

---
+++ client/src/components/ConcertDetail/ConcertDetail.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./ConcertDetail.css"
+
+// This file exports the components for the user's information/My Events page
+
+export function ConcertDetail(props) {
+    return (<>
+        <div className="row">
+            <div className="col-md-6">
+                <div className="card concert-card">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.event.artist}</h5>
+                        <p className="card-text">{props.event.venue} in {props.event.city}</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">On {props.event.date}</li>
+                        <li className="list-group-item">At {props.event.time}</li>
+                        <li className="list-group-item">{props.concert.length} buddies attending!</li>
+                        <li className="list-group-item">tidbit about the band?</li>
+
+                    </ul>
+                    <div className="card-body">
+                        <a href="#" className="card-link">Actually, I'm not interested in this concert</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div>
+                    <div class="card map-card">
+                        <img class="card-img-top" src=".../100px180/" alt="Card image cap"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Google Map goes here</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </>
+    );
+}
--- client/src/pages/Concerts.js
+++ client/src/pages/Concerts.js
@@ -3,16 +3,18 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import { Container, Row, Col } from "../components/Container";
+import { ConcertDetail } from "../components/ConcertDetail/ConcertDetail";
 class MyEvents extends Component {
   state = {
     event: {},
+    concerts: []
   };
 
 
   componentDidMount() {
     API.getConcert(this.props.match.params.id)
       .then(res => {
-        this.setState({ event: res.data });
+        this.setState({ event: res.data, concerts: res.data.attendees });
         console.log(this.state.event.artist)
       })
       .catch(err => console.log(err));
@@ -30,12 +32,12 @@ class MyEvents extends Component {
       <Container>
         <Row>
           <Col size="md-12">
-            <h1>{this.state.event.artist}</h1>
+            {/* <h1>{this.state.event.artist} at {this.state.event.venue}</h1>
             <h2>Playing at {this.state.event.venue} at {this.state.event.time}</h2>
-            <h1>What page is this in?  pages/Concerts</h1>
             <h1>See who's interested in buddy'ing up with you!
             <a class="nav-link logo-nav" href="/chat">Click to Enter Chatroom</a>
-            </h1>
+            </h1> */}
+            <ConcertDetail event={this.state.event} concert={this.state.concerts}/>
             
             
           </Col>
