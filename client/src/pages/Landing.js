71a47aa5908c7c9798e856bda306da2685095aab
richcoleman <coleman.richardm@gmail.com>
2019-03-01 21:54:16 -0600

    changes to landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -53,7 +53,7 @@ class Landing extends Component {
                     color='white'
                     backgroundImage="./photos/edwin-pic.jpg"
                     bg='black'
-                    bgOpacity={0.2}
+                    bgOpacity={0.5}
                 >
                     <Heading
                         font='Major Mono Display'
@@ -66,14 +66,38 @@ class Landing extends Component {
                     <Subhead
                         className='subhead'
                         font='Arial'
-                        fontSize={24}>don't ride solo to the show!
+                        fontSize={20}>Don't ride solo to the show. Find your ConcertBuddy.
                     </Subhead>
-                    <Flex mt={3}>
-                        <CallToAction className='buttons' href='/signup' font={'Arial'} bg='grey' mr={5} color='white'>SIGN UP</CallToAction>
-                        <CallToAction className='buttons' href='/login' font={'Arial'} color='white'>LOG IN</CallToAction>
-                    </Flex>
-                    <ScrollDownIndicator />
+                    <Flex mt={2}>
+                        <CallToAction className='buttons' href='/signup' font={'Arial'} bg='grey' mr={5} color='white'>sign up</CallToAction>
+                        <CallToAction className='buttons' href='/login' font={'Arial'} color='white'>log in</CallToAction>
+                        <ScrollDownIndicator style={{fontWeight: "500"}} />
+
+                    </Flex>                    
                 </Hero>
+                <p className="text-center" style={{fontSize: "4em"}}>What is ConcertBuddy?</p>
+
+
+                    <br></br>
+           
+                    <Subhead
+                        className='subhead'
+                        font='Arial'
+                        fontSize={20}>Have you ever heard had to fly solo to the show? You know what we mean. Maybe that experimental noise-rock duo from Brooklyn is on tour, but your friends aren't really up for ... ahem, "trying new things". Or maybe you're new in town. No friends yet, but you're dying to check out that new Electro-House DJ. But go alone? Feels kind of ... lame, right?
+                         <br></br>
+                        <br></br>
+                        Luckily, you've got ConcertBuddy. Just sign up and join the social network for people like you: music-lovers who aren't afraid to check out shows solo, but wouldn't mind hanging with an equally cool aficionado.
+                        <br></br>
+                        So what are you waiting for? Find your ConcertBuddy.
+                        <br></br>
+                        <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    </Subhead>
             </Provider>
 
         )
